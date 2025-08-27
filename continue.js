4fb174b88a80e2bd82e1af5b4e0e68e6a9aeda41
sohanur24059 <sohanurrahman24059@gmail.com>
2025-08-27 23:02:51 +0600

    send this

---
+++ continue.js
@@ -0,0 +1,20 @@
+let i;
+for(i=1; i<=20; i++){
+    if(i%2==1)
+    {
+        continue
+    }
+    console.log(i)
+}
+
+let n=0;
+while(n <50)
+{
+    n++
+    if(n%5 !==0)
+    {
+        continue
+    }
+    console.log(n)
+    
+}
